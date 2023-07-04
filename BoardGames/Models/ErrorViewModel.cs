2a88dfa487ccf04ac77573d4f972d760c3a9945e
to-matsui0505 <matsui.t@darusu.sakura.ne.jp>
2023-07-04 20:27:20 +0900

    新規プロジェクト追加

---
+++ BoardGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoardGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
